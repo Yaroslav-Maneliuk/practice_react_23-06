70ca5f69ff62f40a5316879553164e79c47266d7
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-06-23 10:24:14 +0300

    Add some logic and style for InputForCounter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import MyCounter from "./components/CounterSection/MyCounter";
+import InputForCounter from "./components/CounterSection/InputForCounter";
 
 const App = () => {
   return (
     <>
       <MyCounter />
+      <InputForCounter />
     </>
   );
 };
